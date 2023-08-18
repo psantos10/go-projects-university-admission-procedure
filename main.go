cc57ce1dcbc7ce649459765db6f4cd0d87ccf360
Patrício dos Santos <hello@psantos.dev>
2023-08-18 18:53:25 +0100

    Stage 1-7: No one is left behind

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var score1, score2, score3 float64
+
+	fmt.Scan(&score1)
+	fmt.Scan(&score2)
+	fmt.Scan(&score3)
+
+	average := (score1 + score2 + score3) / 3
+	fmt.Println(average)
+
+	fmt.Println("Congratulations, you are accepted!")
 }
